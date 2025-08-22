bca9a43d523fdece8b4fea3e816af2cb82295d0a
CDX <80155611+CDX-1@users.noreply.github.com>
2025-08-21 21:50:02 -0400

    Suppress theme-related hydration warnings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-'use client';
-
-import { prompt } from "@/lib/llama";
+import Spotlight from "@/components/spotlight";
 
 export default function Home() {
-	const b = () => {
-		const c = async () => {
-			console.log("Prompting");
-			console.log("=====================");
-			const res = await prompt('gemma3:1b', "What are some fruits that are mistaken for vegetables?");
-			console.log(res);
-			console.log("=====================");
-		}
-
-        c();
-	}
-
-	return (
-		<div>
-			<button onClick={b} className="bg-red-500 px-2 py-1 hover:bg-red-700">Test</button>
-		</div>
-	);
+	return <Spotlight />;
 }
---
+++ src/components/spotlight.tsx
@@ -0,0 +1,9 @@
+import { Input } from "./ui/input";
+
+export default function Spotlight() {
+    return (
+        <div>
+            <Input />
+        </div>
+    );
+}
